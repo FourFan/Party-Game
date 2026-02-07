93625b58f076dded3656e7e48f0b9b889074152b
BuildTools <unconfigured@null.spigotmc.org>
2026-02-08 01:29:14 +0800

    v1.6.1: home icon, kicked player reconnect pre-check, failure message

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1610434102"
+    serviceWorkerVersion: "2371474010"
   }
 });
