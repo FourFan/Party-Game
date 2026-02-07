94f6be6ecb41178a37185956b25111ff144fffb6
BuildTools <unconfigured@null.spigotmc.org>
2026-02-08 04:30:34 +0800

    v1.6.3: restore quizzy kick with implementation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "410472135"
+    serviceWorkerVersion: "3571856447"
   }
 });
