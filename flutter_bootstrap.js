7634bacaf1a73674948c966f01b68e7e9a69ad9c
BuildTools <unconfigured@null.spigotmc.org>
2026-05-19 22:35:25 +0800

    v1.9.5: unify guess mode others card with year mode (_buildFieldLine)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1367552945"
+    serviceWorkerVersion: "3238988557"
   }
 });
